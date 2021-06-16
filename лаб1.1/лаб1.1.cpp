b84bd4658c47f59367958b24a321e65a101f2e40
EvilCookie1005 <82044440+EvilCookie1005@users.noreply.github.com>
2021-06-16 22:48:29 +0500

    Add files via upload

---
+++ лаб1.1/лаб1.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double a, b, c, t;
+    cout << "Введите a:\n";
+    cin >> a;
+    cout << "Введите b:\n";
+    cin >> b;
+    cout << "Введите c:\n";
+    cin >> c;
+
+    double* aPtr = &a;
+    double* bPtr = &b;
+    double* cPtr = &c;
+    double* tPtr = &t;
+    t = c * 3;
+    a = c;
+    cout << "c: " << t << endl;
+    cout << "a: " << a << endl;
+
+    return 0;
+}
